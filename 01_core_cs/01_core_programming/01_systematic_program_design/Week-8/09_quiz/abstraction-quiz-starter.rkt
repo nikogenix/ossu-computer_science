3d46133b040183551ef38deb8246bc0ad3f1b568
nikogenix <nikogenix@gmail.com>
2025-02-08 14:48:30 +0200

    Solve 8

--- 01_core_cs/01_core_programming/01_systematic_program_design/Week-8/09_quiz/abstraction-quiz-starter.rkt
+++ 01_core_cs/01_core_programming/01_systematic_program_design/Week-8/09_quiz/abstraction-quiz-starter.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname abstraction-quiz-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
 ;  PROBLEM 1:
 ;
@@ -15,13 +18,13 @@
               (above (circle 30 "outline" "black") (circle 50 "outline" "black") (circle 70 "outline" "black")))
 
 ;(define (above-all loi) empty-image)  ;stub
-
+#;
 (define (above-all loi)
   (cond [(empty? loi) empty-image]
         [else
          (above (first loi)
                 (above-all (rest loi)))]))
-
+(define (above-all loi) (arrange-all above loi))
 
 
 ;; (listof Image) -> Image
@@ -33,15 +36,20 @@
               (beside (circle 10 "outline" "red") (circle 20 "outline" "blue") (circle 10 "outline" "yellow")))
 
 ;(define (beside-all loi) empty-image)  ;stub
-
+#;
 (define (beside-all loi)
   (cond [(empty? loi) (rectangle 0 0 "solid" "white")]
         [else
          (beside (first loi)
                  (beside-all (rest loi)))]))
+(define (beside-all loi) (arrange-all beside loi))
 
-
-
+;; function that arranges a list of images based on fn
+(define (arrange-all fn loi)
+  (cond [(empty? loi) empty-image]
+        [else
+         (fn (first loi)
+             (arrange-all fn (rest loi)))]))
 
 
 ;  PROBLEM 2:
@@ -58,7 +66,7 @@
 (check-expect (lengths empty) empty)
 (check-expect (lengths (list "apple" "banana" "pear")) (list 5 6 4))
 
-(define (lengths lst) empty)
+(define (lengths lst) (map string-length lst))
 
 
 
@@ -70,7 +78,7 @@
 (check-expect (odd-only empty) empty)
 (check-expect (odd-only (list 1 2 3 4 5)) (list 1 3 5))
 
-(define (odd-only lon) empty)
+(define (odd-only lon) (filter odd? lon))
 
 
 
@@ -83,7 +91,7 @@
 (check-expect (all-odd? (list 1 2 3 4 5)) false)
 (check-expect (all-odd? (list 5 5 79 13)) true)
 
-(define (all-odd? lon) empty)
+(define (all-odd? lon) (andmap odd? lon))
 
 
 
@@ -96,7 +104,11 @@
 (check-expect (minus-n (list 4 5 6) 1) (list 3 4 5))
 (check-expect (minus-n (list 10 5 7) 4) (list 6 1 3))
 
-(define (minus-n lon n) empty)
+(define (minus-n lon n)
+  (local [(define (sub-n x) (- x n))]
+    (map sub-n lon)
+    )
+  )
 
 
 
@@ -107,7 +119,7 @@
 ;  PROBLEM 3
 ;
 ;  Consider the data definition below for Region. Design an abstract fold function for region,
-;  and then use it do design a function that produces a list of all the names of all the
+;  and then use it to design a function that produces a list of all the names of all the
 ;  regions in that region.
 ;
 ;  For consistency when answering the multiple choice questions, please order the arguments in your
@@ -157,4 +169,18 @@
                   [else
                    (... (fn-for-region (first lor))
                         (fn-for-lor (rest lor)))]))]
+    (fn-for-region r)))
+
+(check-expect (all-regions CANADA) (list "Canada" "British Columbia" "Vancouver" "Victoria" "Alberta" "Calgary" "Edmonton"))
+
+(define (all-regions r)
+  (local
+    [(define (fn-for-region r)
+       (cons (region-name r)
+             (fn-for-lor (region-subregions r))))
+     (define (fn-for-lor lor)
+       (cond [(empty? lor) empty]
+             [else
+              (append (fn-for-region (first lor))
+                      (fn-for-lor (rest lor)))]))]
     (fn-for-region r)))
